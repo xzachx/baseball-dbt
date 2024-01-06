5d9672f6b30ee95c1fa56bc26f4b9521f1074e6a
Zach Brown <xzachx@gmail.com>
2024-01-06 13:44:54 +0000

    Cleanup

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,8 +1,8 @@
 with payments as (
     select 
         id as payment_id,
-        order_id,
-        payment_method,
+        orderid as order_id,
+        paymentmethod as payment_method,
         {{ cents_to_dollars('amount') }} as amount
     from {{ source('stripe', 'payments')}}
 )
